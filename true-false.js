48241c46b587b0b18bd4bc4f56046eebb7b71c6e
Md Mahamodul Islam <33100880+mahamoodoul@users.noreply.github.com>
2020-03-07 23:34:59 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,30 @@
+//0
+//""
+//undefined
+//null
+//nan
+//those are falsy value.
+
+//truthy value
+//'0',' ',[]
+
+
+
+
+const age=0;
+//If age=0 then if condition will take it as false
+if(age){
+    console.log("Number True");
+}
+else{
+    console.log("Number Is False");
+}
+
+const name="Salman";
+//string is true other than empty string
+if(name){
+    console.log("Number True");
+}
+else{
+    console.log("Number Is False");
+}
