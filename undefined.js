6681a18d865055dd7b26ed7d6971edbb7897503e
Md Mahamodul Islam <33100880+mahamoodoul@users.noreply.github.com>
2020-03-07 23:52:55 +0600

    null vs undefined

---
+++ undefined.js
@@ -0,0 +1,21 @@
+// let value;
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+
+// const user={name:"shakil",phone:1234};
+// console.log(user.gf); //undefined
+
+// const result= add(13,82);
+//this is undefined as there is no return value
+// console.log(result);
+
+
+let fun=undefined;
+console.log(fun);
+
+let age=[10,11,12];
