5cc2f5da3edfcbdaf0e044223c246f7570c62abc
Md Mahamodul Islam <33100880+mahamoodoul@users.noreply.github.com>
2020-03-08 19:34:35 +0600

    slice-splice-join

---
+++ arrayslice.js
@@ -0,0 +1,10 @@
+const nums=[1,2,3,4,5,6,7];
+// const part=nums.slice(3,5);
+
+const removed=nums.splice(2,5,10,20); //total 5 ta shorabe
+// console.log(removed);
+// console.log(nums);
+
+
+const together=nums.join(",");
+console.log(together);
